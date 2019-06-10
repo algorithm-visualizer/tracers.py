bdb8a091ce2e89624ee6ea8c9012664946ce4c8f
MarkKoz <KozlovMark@gmail.com>
2019-06-09 20:03:23 -0700

    Implement LogTracer

--- algorithm_visualizer/tracers/log.py
+++ algorithm_visualizer/tracers/log.py
@@ -1,4 +1,14 @@
-from algorithm_visualizer import Tracer
+from algorithm_visualizer import Tracer, _Serializable
 
 class LogTracer(Tracer):
-    pass
+    def set(self, log: _Serializable):
+        self.command("set", log)
+
+    def print(self, message: _Serializable):
+        self.command("print", message)
+
+    def println(self, message: _Serializable):
+        self.command("println", message)
+
+    def printf(self, format: str, *args: _Serializable):
+        self.command("printf", format, *args)
