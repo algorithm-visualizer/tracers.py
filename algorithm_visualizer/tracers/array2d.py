e7d0a2c55eb2a5c7b4bd108f93be84708522bc0c
MarkKoz <KozlovMark@gmail.com>
2019-06-10 16:50:05 -0700

    Implement Array2DTracer

--- algorithm_visualizer/tracers/array2d.py
+++ algorithm_visualizer/tracers/array2d.py
@@ -1,4 +1,41 @@
-from algorithm_visualizer import Tracer
+from typing import List
+
+from algorithm_visualizer import Tracer, _Serializable
 
 class Array2DTracer(Tracer):
-    pass
+    def set(self, array2d: List[List[_Serializable]] = None):
+        if array2d is None:
+            array2d = []
+        self.command("set", array2d)
+
+    def patch(self, x: int, y: int, v: _Serializable = None):
+        self.command("patch", x, y, v)
+
+    def depatch(self, x: int, y: int):
+        self.command("depatch", x, y)
+
+    def select(self, sx: int, sy: int, ex: int = None, ey: int = None):
+        if ex is None:
+            ex = sx
+        if ey is None:
+            ey = sy
+        self.command("select", sx, sy, ex, ey)
+
+    def selectRow(self, x: int, sy: int, ey: int):
+        self.command("selectRow", x, sy, ey)
+
+    def selectCol(self, y: int, sx: int, ex: int):
+        self.command("selectCol", y, sx, ex)
+
+    def deselect(self, sx: int, sy: int, ex: int = None, ey: int = None):
+        if ex is None:
+            ex = sx
+        if ey is None:
+            ey = sy
+        self.command("deselect", sx, sy, ex, ey)
+
+    def deselectRow(self, x: int, sy: int, ey: int):
+        self.command("deselectRow", x, sy, ey)
+
+    def deselectCol(self, y: int, sx: int, ex: int):
+        self.command("deselectCol", y, sx, ex)
